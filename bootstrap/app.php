34894df33a366efc71cebd2397d6b6c3226174ec
miiiso1983 <mustafaalrawan@gmail.com>
2025-07-02 23:23:54 +0300

    إصلاح مشكلة Target class [config] does not exist - إضافة Service Providers المفقودة

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,14 +23,26 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
-// تسجيل Service Providers المطلوبة
+// تسجيل جميع Service Providers المطلوبة بالترتيب الصحيح
+$app->register(Illuminate\Foundation\Providers\FoundationServiceProvider::class);
+$app->register(Illuminate\Cache\CacheServiceProvider::class);
 $app->register(Illuminate\Database\DatabaseServiceProvider::class);
-$app->register(Illuminate\Filesystem\FilesystemServiceProvider::class);
-$app->register(Illuminate\View\ViewServiceProvider::class);
-$app->register(Illuminate\Session\SessionServiceProvider::class);
-$app->register(Illuminate\Cookie\CookieServiceProvider::class);
 $app->register(Illuminate\Encryption\EncryptionServiceProvider::class);
+$app->register(Illuminate\Filesystem\FilesystemServiceProvider::class);
+$app->register(Illuminate\Session\SessionServiceProvider::class);
+$app->register(Illuminate\View\ViewServiceProvider::class);
+$app->register(Illuminate\Cookie\CookieServiceProvider::class);
 $app->register(Illuminate\Validation\ValidationServiceProvider::class);
 $app->register(Illuminate\Auth\AuthServiceProvider::class);
+$app->register(Illuminate\Translation\TranslationServiceProvider::class);
+$app->register(Illuminate\Hashing\HashServiceProvider::class);
+
+// تسجيل Application Service Providers
+if (class_exists(App\Providers\AppServiceProvider::class)) {
+    $app->register(App\Providers\AppServiceProvider::class);
+}
+if (class_exists(App\Providers\RouteServiceProvider::class)) {
+    $app->register(App\Providers\RouteServiceProvider::class);
+}
 
 return $app;
